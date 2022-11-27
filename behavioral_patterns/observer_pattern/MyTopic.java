2c9b30340400a67e0a5df862812da110e8a18614
Anthony Kim <fbssaejun@gmail.com>
2022-11-27 15:43:05 -0500

    fix linting on MyTopic file

--- behavioral_patterns/observer_pattern/MyTopic.java
+++ behavioral_patterns/observer_pattern/MyTopic.java
@@ -1,56 +1,58 @@
 package observer_pattern;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyTopic implements Subject {
 
-	private List<Observer> observers;
-	private String message;
-	private boolean changed;
+  private List<Observer> observers;
+  private String message;
+  private boolean changed;
 
-	public MyTopic(){
-		this.observers=new ArrayList<>();
-	}
-	@Override
-	public void register(Observer obj) {
-		if(obj == null) {
+  public MyTopic() {
+    this.observers = new ArrayList<>();
+  }
+
+  @Override
+  public void register(Observer obj) {
+    if (obj == null) {
       throw new NullPointerException("Null Observer");
     }
-		if(!observers.contains(obj)) {
+    if (!observers.contains(obj)) {
       observers.add(obj);
     }
     obj.setSubject(this);
-	}
+  }
 
-	@Override
-	public void unregister(Observer obj) {
-		observers.remove(obj);
-	}
+  @Override
+  public void unregister(Observer obj) {
+    observers.remove(obj);
+  }
 
-	@Override
-	public void notifyObservers() {
-		List<Observer> observersLocal = null;
+  @Override
+  public void notifyObservers() {
+    List<Observer> observersLocal = null;
     if (!changed) {
       return;
     }
     observersLocal = new ArrayList<>(this.observers);
 
-		for (Observer obj : observersLocal) {
-			obj.update("updated!");
-		}
-    this.changed=false;
-	}
+    for (Observer obj : observersLocal) {
+      obj.update("updated!");
+    }
+    this.changed = false;
+  }
 
-	@Override
-	public Object getUpdate(Observer obj) {
-		return this.message;
-	}
+  @Override
+  public Object getUpdate(Observer obj) {
+    return this.message;
+  }
 
-	//method to post message to the topic
-	public void postMessage(String msg){
-		System.out.println("Message Posted to Topic:"+msg);
-		this.message=msg;
-		this.changed=true;
-		notifyObservers();
-	}
+  // method to post message to the topic
+  public void postMessage(String msg) {
+    System.out.println("Message Posted to Topic:" + msg);
+    this.message = msg;
+    this.changed = true;
+    notifyObservers();
+  }
 }
